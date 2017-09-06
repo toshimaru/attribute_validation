7cf042cad7166af9bcb175ddbd287bba5fed7068
toshimaru <me@toshimaru.net>
2017-09-06 18:38:31 +0900

    WIP: create users table and model

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,21 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "attribute_validation"
+# frozen_string_literal: true
 
-require "minitest/autorun"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'attribute_validation'
+
+require 'minitest/autorun'
+
+require 'sqlite3'
+
+DB_NAME = 'test.db'
+db = SQLite3::Database.new DB_NAME
+db.execute <<-SQL
+  create table users(id, name);
+SQL
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: DB_NAME
+)
+
+class User < ActiveRecord::Base
+end
